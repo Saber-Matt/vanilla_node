e08b83c2b0c0755c06b5a0a1f56d8102a4c17483
Matthew Saber <mattsaber21@yahoo.com>
2021-11-05 14:05:46 -0700

    req, res working w/regex- connection successful

--- index.js
+++ index.js
@@ -5,13 +5,30 @@
 
 //Dependencies
 const http = require('http');
+const url = require('url');
 
 //Server should respond to all requests with a string
 const server = http.createServer(function(req, res){
-  res.end('Hello World\n');
+  
+
+
+// Get URL and parse it
+const parsedUrl = url.parse(req.url, true);
+
+//Get the path
+const path = parsedUrl.pathname;
+//trim path with regex
+const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+//Send the response
+res.end('Hello World\n');
+
+//Log the request path
+console.log('Request received on path: '+trimmedPath);
+
 });
 
 //Start server and listen on port 3000
 server.listen(3000,function(){
   console.log('The server is listening on port 3000 now');
-})
+});
